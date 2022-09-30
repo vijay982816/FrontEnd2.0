2790c74dcac1a641365d2dbb771ded9666c6f9cc
vijay982816 <vijay982816vijay@gmail.com>
2022-09-30 08:20:13 +0000

    changed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from "path-browserify"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      path: "path-browserify",
+    },
   }
 })
+
+
+
